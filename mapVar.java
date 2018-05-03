f9507a15afb58a01238118c405e775830c95569b
F4N4T1C <36743489+F4N4T1C@users.noreply.github.com>
2018-05-03 15:08:45 -0400

    Add files via upload

---
+++ mapVar.java
@@ -0,0 +1,25 @@
+package com.company;
+
+/**
+ * Created by Robert Hubert on 21/03/2018.
+ */
+public class mapVar {
+
+    // ---------*---------*---------*---------*---------*
+    // Integer Constants
+    public static final int INVALID  = -1;
+
+
+    public static final int ROW = 200;
+    public static final int COL = 200;
+
+
+
+
+
+    private mapVar(){
+        //this prevents even the native class from calling this constructor as well
+        throw new AssertionError();
+    }
+
+}
---
+++ mappingRobot.java
@@ -0,0 +1,8 @@
+package com.company;
+
+/**
+ * Created by 93851kel on 03/05/2018.
+ */
+public class mappingRobot {
+
+}
